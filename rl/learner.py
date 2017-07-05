dd4950eb46d80427a15d75d0aa3be02cfa8c2e78
jon <jonathanhon@hotmail.com>
2017-07-05 21:38:40 +0100

    Added Q learner.

--- rl/learner.py
+++ rl/learner.py
@@ -17,9 +17,44 @@ class Learner:
         self.rl_system.action_value_function.fit(state_array, targets, **kwargs)
 
     def get_targets(self, episode):
+        """
+        Generates the target values for training
+        :param episode:
+        :return: np.ndarray(len(episodes), num_actions
+        """
+        raise NotImplemented()
+
+
+class RewardLearner(Learner):
+
+    def get_targets(self, episode):
         targets = np.zeros((len(episode.states), self.rl_system.num_actions))
         rewards = episode.get_reward_array()
         actions = episode.get_action_array()
         for i, (action, reward) in enumerate(zip(actions, rewards)):
             targets[i, action] = reward
+        return targets
+
+
+class QLearner(Learner):
+
+    def __init__(self, rl_system, gamma=1.0):
+        self.rl_system = rl_system
+        self.gamma = gamma
+
+    def get_targets(self, episode):
+        targets = np.zeros((len(episode.states), self.rl_system.num_actions))
+        rewards = episode.get_reward_array()
+
+        for i, state in enumerate(episode.states):
+            action = episode.actions[i]
+            next_state = self.rl_system.model.apply_action(state, action)
+
+            next_state_vector = next_state.as_vector().reshape((1, -1))
+            next_state_action_values = self.rl_system.action_value_function(next_state_vector)
+
+            state_vector = state.as_vector().reshape(1, -1)
+            targets[i, :] = self.rl_system.action_value_function(state_vector)
+            targets[i, action] = rewards[i] + self.gamma * next_state_action_values.max()
+
         return targets
--- rl/tests/learner_tests.py
+++ rl/tests/learner_tests.py
@@ -2,7 +2,7 @@ import unittest
 import logging
 
 from rl.grid_world import GridState
-from rl.learner import Learner
+from rl.learner import Learner, RewardLearner, QLearner
 from rl.grid_world import GridRewardFunction, GridWorld, GridState
 from rl.policy import EquiProbableRandomPolicy
 from rl.value import ValueFunction
@@ -14,8 +14,7 @@ N = 1000
 logging.basicConfig(level=logging.DEBUG)
 
 
-# class EpisodeTests(unittest.TestCase):
-class EpisodeTests(np.testing.TestCase):
+class RewardLearnerTests(np.testing.TestCase):
 
     def test_learn_episode(self):
         states = [GridState((1, 1)), GridState((2, 2)), GridState((3, 3))]
@@ -26,19 +25,45 @@ class EpisodeTests(np.testing.TestCase):
         grid_world = GridWorld()
 
         y = grid_world.action_value_function(episode.get_state_array())
-        logging.info('initial targets are:')
+        logging.info('Reward Learner initial targets are:')
         logging.info('\n' + str(y))
 
-        learner = Learner(grid_world)
+        learner = RewardLearner(grid_world)
         learner.learn_episode(episode)
 
         y = grid_world.action_value_function(episode.get_state_array())
-        logging.info('fitted targets are:')
+        logging.info('Reward Learner fitted targets are:')
         logging.info('\n' + str(y))
 
         np.testing.assert_allclose([0, -1, 0, 0], y[0], atol=0.5)
-        np.testing.assert_allclose([0, 0, -1, 0], y[0], atol=0.5)
-        np.testing.assert_allclose([0, 0, 0, 0], y[0], atol=0.5)
+        np.testing.assert_allclose([0, 0, -1, 0], y[1], atol=0.5)
+        np.testing.assert_allclose([0, 0, 0, 0], y[2], atol=0.5)
+
+
+class QLearnerTests(unittest.TestCase):
+
+    def test_learn_episode(self):
+        states = [GridState((1, 1)), GridState((2, 2)), GridState((3, 3))]
+        rewards = [-1, -1, 0]
+        actions = [1, 2, 3]
+        episode = Episode(states, actions, rewards)
+
+        grid_world = GridWorld()
+
+        y = grid_world.action_value_function(episode.get_state_array())
+        logging.info('Q Learning initial targets are:')
+        logging.info('\n' + str(y))
+
+        learner = QLearner(grid_world)
+        learner.learn_episode(episode)
+
+        y = grid_world.action_value_function(episode.get_state_array())
+        logging.info('Q Learning fitted targets are:')
+        logging.info('\n' + str(y))
+
+        np.testing.assert_allclose([0, -1, 0, 0], y[0], atol=0.5)
+        np.testing.assert_allclose([0, 0, -1, 0], y[1], atol=0.5)
+        np.testing.assert_allclose([0, 0, 0, 0], y[2], atol=0.5)
 
 
 def get_expected_vector(player):
