7f1583d0e16f932c2defd4aafc1740c95971137f
jon <jonathanhon@hotmail.com>
2017-07-06 22:37:12 +0100

    created the mock system, and done some tests with it

--- rl/learner.py
+++ rl/learner.py
@@ -11,7 +11,7 @@ class Learner:
         self.rl_system = rl_system
 
     def learn_episode(self, episode, **kwargs):
-        state_array = episode.get_state_array()
+        state_array = episode.get_state_array()[:-1]
         targets = self.get_targets(episode)
         logging.info('Targets are:\n%s' % str(targets))
         self.rl_system.action_value_function.fit(state_array, targets, **kwargs)
@@ -28,7 +28,7 @@ class Learner:
 class RewardLearner(Learner):
 
     def get_targets(self, episode):
-        targets = np.zeros((len(episode.states), self.rl_system.num_actions))
+        targets = np.zeros((len(episode.states)-1, self.rl_system.num_actions))
         rewards = episode.get_reward_array()
         actions = episode.get_action_array()
         for i, (action, reward) in enumerate(zip(actions, rewards)):
@@ -43,12 +43,44 @@ class QLearner(Learner):
         self.gamma = gamma
 
     def get_targets(self, episode):
+        targets = np.zeros((len(episode.states)-1, self.rl_system.num_actions))
+        rewards = episode.get_reward_array()
+
+        for i, state in enumerate(episode.states[:-1]):
+            action = episode.actions[i]
+            next_state = self.rl_system.model.apply_action(state, action)
+
+            next_state_vector = next_state.as_array().reshape((1, -1))
+            next_state_action_values = self.rl_system.action_value_function(next_state_vector)
+
+            state_vector = state.as_array().reshape(1, -1)
+            targets[i, :] = self.rl_system.action_value_function(state_vector)
+            targets[i, action] = rewards[i] + self.gamma * next_state_action_values.max()
+
+        return targets
+
+
+class WQLearner(Learner):
+
+    def __init__(self, rl_system, gamma=1.0):
+        self.rl_system = rl_system
+        self.gamma = gamma
+
+    def get_targets(self, episode):
         targets = np.zeros((len(episode.states), self.rl_system.num_actions))
         rewards = episode.get_reward_array()
 
         for i, state in enumerate(episode.states):
             action = episode.actions[i]
-            next_state = self.rl_system.model.apply_action(state, action)
+
+            next_state_action_values = np.ndarray((self.rl_system.num_actions, self.rl_system.num_actions))
+
+            for action in range(self.rl_system.num_actions):
+                next_state = self.rl_system.model.apply_action(state, action)
+                next_state_vector = next_state.as_vector().reshape((1, -1))
+                next_state_action_values[action, :] = self.rl_system.action_value_function(next_state_vector)
+
+            #CONTINUE TOOO TIRED
 
             next_state_vector = next_state.as_vector().reshape((1, -1))
             next_state_action_values = self.rl_system.action_value_function(next_state_vector)
--- rl/tests/learner_tests.py
+++ rl/tests/learner_tests.py
@@ -1,13 +1,14 @@
-import unittest
 import logging
+import unittest
 
-from rl.grid_world import GridState
-from rl.learner import Learner, RewardLearner, QLearner
-from rl.grid_world import GridRewardFunction, GridWorld, GridState
-from rl.policy import EquiProbableRandomPolicy
-from rl.value import ValueFunction
-from rl.experience import ExperienceGenerator, Episode
 import numpy as np
+
+from rl.core import RLSystem, State
+from rl.experience import Episode
+from rl.grid_world import GridWorld, GridState
+from rl.learner import RewardLearner, QLearner
+from rl.policy import Policy
+from rl.value import ActionValueFunction
 
 N = 1000
 
@@ -18,8 +19,8 @@ class RewardLearnerTests(np.testing.TestCase):
 
     def test_learn_episode(self):
         states = [GridState((1, 1)), GridState((2, 2)), GridState((3, 3))]
-        rewards = [-1, -1, 0]
-        actions = [1, 2, 3]
+        rewards = [-1, -1]
+        actions = [1, 2]
         episode = Episode(states, actions, rewards)
 
         grid_world = GridWorld()
@@ -39,13 +40,29 @@ class RewardLearnerTests(np.testing.TestCase):
         np.testing.assert_allclose([0, 0, -1, 0], y[1], atol=0.5)
         np.testing.assert_allclose([0, 0, 0, 0], y[2], atol=0.5)
 
+    def test_get_targets(self):
+
+        mock_system = MockSystem()
+        learner = RewardLearner(mock_system)
+
+        states = [MockState1(), MockState1A(), MockState2()]
+        actions = [0, 0]
+        rewards = [0, 0]
+        episode = Episode(states=states, actions=actions, rewards=rewards)
+
+        targets = learner.get_targets(episode)
+        logging.info('get targets = %s' % str(targets))
+
+        expected = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
+        np.testing.assert_almost_equal(expected, targets)
+
 
 class QLearnerTests(unittest.TestCase):
 
     def test_learn_episode(self):
         states = [GridState((1, 1)), GridState((2, 2)), GridState((3, 3))]
-        rewards = [-1, -1, 0]
-        actions = [1, 2, 3]
+        rewards = [-1, -1]
+        actions = [1, 2]
         episode = Episode(states, actions, rewards)
 
         grid_world = GridWorld()
@@ -66,12 +83,93 @@ class QLearnerTests(unittest.TestCase):
         np.testing.assert_allclose([0, 0, 0, 0], y[2], atol=0.5)
 
 
+class MockSystem(RLSystem):
+    def __init__(self):
+        super(MockSystem, self).__init__()
+        self.num_actions = 3
+        self.model = MockModel()
+        self.policy = MockPolicy(self.num_actions)
+        self.value_function = MockActionValueFunction(self.policy)
+
+
+class MockPolicy(Policy):
+
+    def __call__(self, state):
+        return np.array([1.0, 0.0, 0.0])
+
+
+class MockActionValueFunction(ActionValueFunction):
+    def __call__(self, state):
+
+        if isinstance(state, MockState1A):
+            return 1
+        elif isinstance(state, MockState1B):
+            return 2
+        elif isinstance(state, MockState1C):
+            return 3
+        elif isinstance(state, MockState2):
+            return 0
+        else:
+            raise ValueError('State not expected %s' % str(state))
+
+
+class MockModel(object):
+
+    def apply_action(self, state, action):
+
+        if isinstance(state, MockState1):
+            if action == 0:
+                return MockState1A()
+            elif action == 1:
+                return MockState1B()
+            elif action == 2:
+                return MockState1C()
+            else:
+                raise ValueError('Expect 0, 1 or 2')
+        elif isinstance(state, (MockState1A, MockState1B, MockState1C)):
+            return MockState2()
+        elif isinstance(state, MockState2):
+            return MockState2()
+        else:
+            raise ValueError('object %s not expected' % str(state))
+
+
+class MockState(State):
+    def __init__(self):
+        super(MockState, self).__init__()
+        self.size = 3
+        self.array_dtype = np.bool
+
+
+class MockState1(MockState):
+    def as_array(self):
+        return np.array([False, False, False], np.bool).reshape(1, 3)
+
+
+class MockState1A(MockState):
+    def as_array(self):
+        return np.array([True, False, False], np.bool).reshape(1, 3)
+
+
+class MockState1B(MockState):
+    def as_array(self):
+        return np.array([False, True, False], np.bool).reshape(1, 3)
+
+
+class MockState1C(MockState):
+    def as_array(self):
+        return np.array([False, False, True], np.bool).reshape(1, 3)
+
+
+class MockState2(MockState):
+    def as_array(self):
+        return np.array([True, True, True], np.bool).reshape(1, 3)
+
+
 def get_expected_vector(player):
     result = np.zeros(16, dtype=np.bool)
     result[4 * player[0] + player[1]] = True
     return result
-
-
 
 
 if __name__ == '__main__':
