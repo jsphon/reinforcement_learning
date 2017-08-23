56e15c2f95851b6cca09a2fc5eea7888896bc0f7
jon <jonathanhon@hotmail.com>
2017-08-23 21:43:27 +0100

    Now also have an example for scalar q learning.

--- rl/examples/simple_grid_world.py
+++ rl/examples/simple_grid_world.py
@@ -2,11 +2,10 @@ import logging
 
 import numpy as np
 
-from rl.core.experience import ExperienceGenerator
-from rl.core.learner import QLearner, SarsaLearner, ExpectedSarsaLearner
-from rl.environments.simple_grid_world import SimpleGridWorld
-from rl.core.policy import SoftmaxPolicy
 from rl.lib.timer import Timer
+from rl.core.experience import ExperienceGenerator
+from rl.core.learning.learner import build_q_learner
+from rl.environments.simple_grid_world import SimpleGridWorld
 
 np.set_printoptions(precision=1)
 np.set_printoptions(linewidth=200)
@@ -14,23 +13,22 @@ np.set_printoptions(suppress=True)
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 grid_world = SimpleGridWorld()
+
 generator = ExperienceGenerator(grid_world)
-episode = generator.generate_episode()
 
-learner = QLearner(grid_world)
-learner = SarsaLearner(grid_world)
+learner = build_q_learner(grid_world)
 
-learner = ExpectedSarsaLearner(grid_world)
-learner.rl_system.policy = SoftmaxPolicy(grid_world)
+print('=== Initial Value Function ===')
+print(grid_world.get_value_grid())
 
-with Timer('training') as t:
-    for i in range(100):
-        episode = generator.generate_episode()
-        learner.learn(episode, verbose=0)
-
-        print('=== Value Function %i ===' % i)
-        print(grid_world.get_value_grid())
+learner.gamma = 1.0
+for i in range(20):
+    with Timer('Generating Episodes'):
+        episodes = generator.generate_episodes(100, max_len=10)
+    with Timer('Learning Model'):
+        learner.learn(episodes)
 
 print('=== Reward Function ===')
 print(grid_world.get_reward_grid())
