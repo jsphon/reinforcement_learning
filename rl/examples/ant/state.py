0ab7e20cc81efa108e758316d83001181851ca55
jon <jonathanhon@hotmail.com>
2017-09-21 21:39:10 +0100

    removed dead code

--- rl/examples/ant/state.py
+++ rl/examples/ant/state.py
@@ -1,5 +1,5 @@
 import numpy as np
-from rl.core.state import IntExtState, State
+from rl.core.state import State
 
 
 class AntState(State):
@@ -19,9 +19,6 @@ class AntState(State):
         return '<AntState external_state.position=%s, num_homecomings=%s>' % (
             self.position, self.num_homecomings)
 
-    # def copy(self):
-    #     return AntState(internal_state=self.internal_state, position=self.external_state.position)
-
     def as_array(self):
         values = np.zeros(self.size, dtype=np.bool)
         values[self.position] = 1
@@ -37,33 +34,6 @@ class AntState(State):
         self._num_homecomings = value
         if value == self.max_home_comings:
             self.is_terminal = True
-
-
-#
-# class ExternalState(State):
-#
-#     def __init__(self, position=5):
-#         self.position = position
-#         self.is_terminal=False
-#         self.size = 11
-#         self.array_dtype = np.bool
-#         self._num_homecomings = 0
-#
-#     def as_array(self):
-#         values = np.zeros(self.size, dtype=np.bool)
-#         values[self.position] = 1
-#         values[10] = self._num_homecomings
-#         return values
-#
-#     @property
-#     def num_homecomings(self):
-#         return self._num_homecomings
-#
-#     @num_homecomings.setter
-#     def num_homecomings(self, value):
-#         self._num_homecomings = value
-#         if value == 5:
-#             self.is_terminal=True
 
 
 if __name__ == '__main__':
