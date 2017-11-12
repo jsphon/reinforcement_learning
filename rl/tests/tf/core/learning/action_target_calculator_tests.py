ae5013e88a2fa2a8aa2f351d8d8bd7369c7c93be
jon <jonathanhon@hotmail.com>
2017-11-12 21:53:51 +0000

    We have the sarsa action target calculator converted to tensorflow,but the looping is too slow.

---
+++ rl/tests/tf/core/learning/action_target_calculator_tests.py
@@ -0,0 +1,92 @@
+import logging
+import unittest
+from mock import MagicMock
+from collections import defaultdict
+
+import numpy as np
+import tensorflow as tf
+from rl.lib.timer import Timer
+
+import scipy.stats
+from rl.tf.core.learning.action_target_calculator import \
+    QLearningActionTargetCalculator,\
+    SarsaActionTargetCalculator,\
+    ExpectedSarsaActionTargetCalculator
+
+N = 1000
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class SarsaActionTargetCalculatorTests(unittest.TestCase):
+
+    def test_calculate(self):
+        """
+        Test that calculate_action_target returns targets with the correct distributions
+
+        """
+        rl_system = MagicMock()
+        calculator = SarsaActionTargetCalculator(
+            rl_system=rl_system,
+            discount_factor=tf.Variable(1.0))
+
+        a_probabilities = np.array([0.5, 0.3, 0.2])
+        t_probabilities = tf.Variable(a_probabilities)
+        rl_system.policy.calculate_action_value_probabilities.return_value = t_probabilities
+        action_values = tf.Variable([0.0, 1.0, 2.0])
+        reward = tf.Variable(0.0)
+        t_target = calculator.calculate(reward, action_values)
+
+        counts = defaultdict(float)
+        for _ in range(N):
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+                result = sess.run(t_target)
+            #logging.info(result)
+            counts[result] += 1
+
+        f_exp = N * a_probabilities
+        f_obs = [counts[0.0], counts[1.0], counts[2.0]]
+        self.do_chi_squared_htest(f_obs, f_exp)
+
+    def do_chi_squared_htest(self, f_obs, f_exp):
+        chi = scipy.stats.chisquare(f_obs, f_exp)
+        self.assertTrue(chi.pvalue > 0.01)
+#
+#
+# class QLearningActionTargetCalculatorTests(unittest.TestCase):
+#     def test_calculate(self):
+#         rl_system = MagicMock()
+#         calculator = QLearningActionTargetCalculator(
+#             rl_system=rl_system,
+#             discount_factor=1.0)
+#
+#         reward = 1.0
+#         next_state_action_values = np.array([1.0, 2.0, 3.0])
+#         result = calculator.calculate(reward, next_state_action_values)
+#
+#         self.assertEqual(4.0, result)
+#
+#
+# class ExpectedSarsaActionTargetCalculatorTests(unittest.TestCase):
+#
+#     def test_calculate(self):
+#         rl_system = MagicMock(num_actions=3)
+#         probabilities = np.array([0.5, 0.3, 0.2])
+#         rl_system.policy.calculate_action_value_probabilities.return_value = probabilities
+#
+#         calculator = ExpectedSarsaActionTargetCalculator(
+#             rl_system=rl_system,
+#             discount_factor=1.0)
+#
+#         reward = 1.0
+#         next_state_action_values = np.array([1.0, 2.0, 3.0])
+#         result = calculator.calculate(reward, next_state_action_values)
+#         expected = 1.0 + np.dot(probabilities, next_state_action_values)
+#
+#         self.assertEqual(expected, result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ rl/tf/core/learning/action_target_calculator.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+
+class ActionTargetCalculator(object):
+    """
+    For calculating the target for a single action
+    """
+    def __init__(self, rl_system, discount_factor=1.0):
+        self.rl_system = rl_system
+        self.discount_factor = discount_factor
+
+    def calculate(self, reward, next_state_action_values):
+        raise NotImplemented()
+
+
+class QLearningActionTargetCalculator(ActionTargetCalculator):
+    def calculate(self, reward, next_state_action_values):
+        return reward + self.discount_factor * next_state_action_values.max()
+
+
+class SarsaActionTargetCalculator(ActionTargetCalculator):
+    def calculate(self, reward, next_state_action_values):
+        pi = self.rl_system.policy.calculate_action_value_probabilities(next_state_action_values)
+        #action = np.random.choice(len(pi), p=pi)
+        logits = tf.log(pi)
+        #logits = tf.reshape(logits, (1, -1))
+        action = tf.multinomial([logits], 1)
+        #return reward + self.discount_factor * next_state_action_values[action]
+        next_state_action_value = next_state_action_values[action[0][0]]
+        return reward + self.discount_factor * next_state_action_value
+
+
+class ExpectedSarsaActionTargetCalculator(ActionTargetCalculator):
+    '''
+    target = R_{t+1} + \gamma * \E Q(S_{t+1}, A_{t+1})
+           = R_{t+1} + \gamma * \sum_{a} \pi(a | S_{t+1} Q(S_{t+1}, a)
+
+    where \gamma is the discount factor
+
+    '''
+
+    def calculate(self, reward, next_state_action_values):
+        pi = self.rl_system.policy.calculate_action_value_probabilities(next_state_action_values)
+        num_actions = len(next_state_action_values)
+        pi = pi.reshape((1, num_actions))
+        next_state_action_values = next_state_action_values.reshape((num_actions, 1))
+        expectation = np.dot(pi, next_state_action_values)
+        return reward + self.discount_factor * expectation
