e46fd7bfd87b99f8729e9818cd531779c5a713d3
jon <jonathanhon@hotmail.com>
2017-11-12 22:39:06 +0000

    Removed the old test_calculate.

--- rl/tests/tf/core/learning/action_target_calculator_tests.py
+++ rl/tests/tf/core/learning/action_target_calculator_tests.py
@@ -13,42 +13,42 @@ from rl.tf.core.learning.action_target_calculator import \
     SarsaActionTargetCalculator,\
     ExpectedSarsaActionTargetCalculator
 
-N = 1000
+N = 10000
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 class SarsaActionTargetCalculatorTests(unittest.TestCase):
 
-    def test_calculate(self):
-        """
-        Test that calculate_action_target returns targets with the correct distributions
-
-        """
-        rl_system = MagicMock()
-        calculator = SarsaActionTargetCalculator(
-            rl_system=rl_system,
-            discount_factor=tf.Variable(1.0))
-
-        a_probabilities = np.array([0.5, 0.3, 0.2])
-        t_probabilities = tf.Variable(a_probabilities)
-        rl_system.policy.calculate_action_value_probabilities.return_value = t_probabilities
-        action_values = tf.Variable([0.0, 1.0, 2.0])
-        reward = tf.Variable(0.0)
-        t_target = calculator.calculate(reward, action_values)
-
-        counts = defaultdict(float)
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            for _ in range(N):
-                result = sess.run(t_target)
-                logging.info(result)
-                counts[result] += 1
-
-        f_exp = N * a_probabilities
-        f_obs = [counts[0.0], counts[1.0], counts[2.0]]
-        print(counts)
-        self.do_chi_squared_htest(f_obs, f_exp)
+    # def test_calculate(self):
+    #     """
+    #     Test that calculate_action_target returns targets with the correct distributions
+    #
+    #     """
+    #     rl_system = MagicMock()
+    #     calculator = SarsaActionTargetCalculator(
+    #         rl_system=rl_system,
+    #         discount_factor=tf.Variable(1.0))
+    #
+    #     a_probabilities = np.array([0.5, 0.3, 0.2])
+    #     t_probabilities = tf.Variable(a_probabilities)
+    #     rl_system.policy.calculate_action_value_probabilities.return_value = t_probabilities
+    #     action_values = tf.Variable([0.0, 1.0, 2.0])
+    #     reward = tf.Variable(0.0)
+    #     t_target = calculator.calculate(reward, action_values)
+    #
+    #     counts = defaultdict(float)
+    #     with tf.Session() as sess:
+    #         sess.run(tf.global_variables_initializer())
+    #         for _ in range(N):
+    #             result = sess.run(t_target)
+    #             logging.info(result)
+    #             counts[result] += 1
+    #
+    #     f_exp = N * a_probabilities
+    #     f_obs = [counts[0.0], counts[1.0], counts[2.0]]
+    #     print(counts)
+    #     self.do_chi_squared_htest(f_obs, f_exp)
 
     def do_chi_squared_htest(self, f_obs, f_exp):
         chi = scipy.stats.chisquare(f_obs, f_exp)
@@ -69,12 +69,19 @@ class SarsaActionTargetCalculatorTests(unittest.TestCase):
         rl_system.policy.calculate_action_value_probabilities.return_value = t_probabilities
         action_values = tf.Variable([0.0, 1.0, 2.0])
         reward = tf.Variable(0.0)
-        t_target = calculator.calculate(reward, action_values)
 
-        make_target = lambda:calculator.calculate(reward, action_values)
+        def get_target():
+            return calculator.calculate(reward, action_values)
 
-        samples = self.generate_samples(make_target)
-        print(samples)
+        with Timer('Generating Samples'):
+            samples = self.generate_samples(get_target, num_samples=N)
+
+        itemfreq = scipy.stats.itemfreq(samples)
+        itemfreq = dict(itemfreq)
+
+        f_exp = N * a_probabilities
+        f_obs = [itemfreq[0.0], itemfreq[1.0], itemfreq[2.0]]
+        self.do_chi_squared_htest(f_obs, f_exp)
 
     def generate_samples(self, t, num_samples=1000):
         """
@@ -88,16 +95,16 @@ class SarsaActionTargetCalculatorTests(unittest.TestCase):
         """
 
         i = tf.Variable(0)
-
-        cond = lambda i, t, result: tf.less(i, num_samples)
-
         result = tf.TensorArray(t().dtype, num_samples)
 
-        def body(b_i, b_t, b_result):
-            b_result = b_result.write(b_i, t())
-            return b_i+1, b_t, b_result
+        def cond(b_i, result):
+            return tf.less(b_i, num_samples)
 
-        i, t, result = tf.while_loop(cond, body, (i, t(), result))
+        def body(b_i, b_result):
+            b_result = b_result.write(b_i, t())
+            return b_i+1, b_result
+
+        i, result = tf.while_loop(cond, body, (i, result))
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             return sess.run(result.stack())
