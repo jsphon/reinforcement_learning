d042cdd176f077af91bb827222eab05498180d6b
jon <jonathanhon@hotmail.com>
2017-12-06 21:48:47 +0000

    Added a test for train_op. For some reason train_loop does not work. Analyse it later.

--- rl/tests/tf/core/value_function_tests.py
+++ rl/tests/tf/core/value_function_tests.py
@@ -73,6 +73,74 @@ class OneHotInputTransformTests(unittest.TestCase):
 
         self.ty_true = tf.Variable(ny_true, dtype=tf.float32, trainable=False)
 
+    def test_train_op(self):
+        '''
+        Test that the model is flexible enough to fit
+        our true value function
+        Returns:
+        '''
+
+        y = self.builder.calculate(self.tx)
+        loss = squared_loss(y, self.ty_true)
+
+        train_op = self.builder.train_op(self.tx, self.ty_true, learning_rate=LEARNING_RATE)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            initial_loss, initial_y = sess.run([loss, y])
+
+            with Timer('Training using Python loop with %i steps' % TRAIN_STEPS):
+                for _ in range(TRAIN_STEPS):
+                    sess.run(train_op)
+
+                final_loss, final_y, final_ty_true = sess.run([loss, y, self.ty_true])
+
+        print('initial loss : %0.2f' % initial_loss)
+        print('initial y : %s' % str(initial_y))
+
+        print('final loss : %0.2f' % final_loss)
+        print('final y : %s' % str(final_y))
+
+        self.assertLess(final_loss, initial_loss)
+        self.assertLess(final_loss, 1)
+
+    def test_train_loop(self):
+        '''
+        Test that the model is flexible enough to fit
+        our true value function
+        Returns:
+        '''
+
+        y = self.builder.calculate(self.tx)
+        loss = squared_loss(y, self.ty_true)
+
+        train_loop = self.builder.train_loop(self.tx,
+                                             self.ty_true,
+                                             learning_rate=LEARNING_RATE,
+                                             num_steps=TRAIN_STEPS
+                                             )
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            initial_loss, initial_y = sess.run([loss, y])
+
+            with Timer('Training using train_loop %i steps' % TRAIN_STEPS):
+                sess.run(train_loop)
+
+                final_loss, final_y, final_ty_true = sess.run([loss, y, self.ty_true])
+
+        print('initial loss : %0.2f' % initial_loss)
+        print('initial y : %s' % str(initial_y))
+
+        print('final loss : %0.2f' % final_loss)
+        print('final y : %s' % str(final_y))
+
+        self.assertLess(final_loss, initial_loss)
+        self.assertLess(final_loss, 1)
+
+
     def test_calculate(self):
 
         tx = tf.Variable([1], dtype=tf.int32, trainable=False)
