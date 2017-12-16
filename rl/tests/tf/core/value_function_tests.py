0732dd778a757187f4975ea8dad42b686158650e
jon <jonathanhon@hotmail.com>
2017-12-16 21:48:48 +0000

    Test to show that this will work with the line world, though should we move it out?

--- rl/tests/tf/core/value_function_tests.py
+++ rl/tests/tf/core/value_function_tests.py
@@ -197,6 +197,96 @@ class OneHotInputTransformTests(unittest.TestCase):
             np.testing.assert_array_equal(ey.ravel(), ay[position])
 
 
+class OneHotInputTransformTests2(unittest.TestCase):
+
+    def setUp(self):
+        self.builder = ValueFunctionBuilder(
+            input_shape=10,
+            hidden_shape=[10, 5],
+            output_shape=2,
+            use_one_hot_input_transform=True,
+        )
+
+        nx = np.arange(10)
+        self.tx = tf.Variable(nx, dtype=tf.int32, trainable=False)
+
+        ny_true = np.array([
+            [9, 10, 9, 8 , 7, 6, 5, 4, 3, 2],
+            [7,  8, 9, 10, 9, 8, 7, 6, 5, 4],
+        ]
+        ).T
+
+        self.ty_true = tf.Variable(ny_true, dtype=tf.float32, trainable=False)
+
+    def test_train_op(self):
+        '''
+        Test that the model is flexible enough to fit
+        our true value function
+        Returns:
+        '''
+
+        y = self.builder.calculate(self.tx)
+        loss = squared_loss(y, self.ty_true)
+
+        train_op = self.builder.train_op(self.tx, self.ty_true, learning_rate=LEARNING_RATE)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            initial_loss, initial_y = sess.run([loss, y])
+
+            with Timer('Training using Python loop with %i steps' % TRAIN_STEPS):
+                for _ in range(TRAIN_STEPS):
+                    sess.run(train_op)
+
+                final_loss, final_y, final_ty_true = sess.run([loss, y, self.ty_true])
+
+        print('initial loss : %0.2f' % initial_loss)
+        print('initial y : %s' % str(initial_y))
+
+        print('final loss : %0.2f' % final_loss)
+        print('final y : %s' % str(final_y))
+
+        self.assertLess(final_loss, initial_loss)
+        self.assertLess(final_loss, 1)
+
+    def test_train_loop(self):
+        '''
+        Test that the model is flexible enough to fit
+        our true value function
+        Returns:
+        '''
+
+        y = self.builder.calculate(self.tx)
+        loss = squared_loss(y, self.ty_true)
+
+        train_loop = self.builder.train_loop(self.tx,
+                                             self.ty_true,
+                                             learning_rate=LEARNING_RATE,
+                                             num_steps=TRAIN_STEPS
+                                             )
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            initial_loss, initial_y = sess.run([loss, y])
+
+            with Timer('Training using train_loop %i steps' % TRAIN_STEPS):
+                sess.run(train_loop)
+
+                final_loss, final_y, final_ty_true = sess.run([loss, y, self.ty_true])
+
+        print('initial loss : %0.2f' % initial_loss.round(2))
+        print('initial y : %s' % str(initial_y.round(2)))
+
+        print('final loss : %0.2f' % final_loss.round(2))
+        print('final y : %s' % str(final_y.round(2)))
+
+        self.assertLess(final_loss, initial_loss)
+        self.assertLess(final_loss, 1)
+
+
+
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.builder = ValueFunctionBuilder(
