dfd7af8ed98c14f4ff1f3ed4f93bb03f8518385e
jon <jonathanhon@hotmail.com>
2018-03-23 22:45:30 +0000

    Added RepeatSlowTestCase, which demonstrates how to fit a linear regression model with repeat_func

--- rl/tests/tf/lib/repeat_tests.py
+++ rl/tests/tf/lib/repeat_tests.py
@@ -1,5 +1,5 @@
-
-
+import numpy as np
+from scipy import stats
 import tensorflow as tf
 
 from rl.tf.lib import repeat
@@ -24,3 +24,68 @@ class RepeatTestCase(tf.test.TestCase):
             result = c.eval()
 
         self.assertEqual(10, result)
+
+
+class RepeatSlowTestCase(tf.test.TestCase):
+    """
+    More complete example
+    """
+
+    def test_linear_regression(self):
+        n = 1000
+
+        a = 2.0
+        b = 1.0
+
+        np.random.seed(0)
+        aX = np.random.uniform(0, 10, n)
+        ay = a * aX + b + np.random.standard_normal(n)
+
+        # Check results
+        slope, intercept, r_value, p_value, std_err = stats.linregress(aX, ay)
+
+        print('slope : %0.2f' % slope)
+        print('intercept : %0.2f' % intercept)
+
+        tX = tf.Variable(aX, trainable=False)
+        ty = tf.Variable(ay, trainable=False)
+
+        va = tf.Variable(0.0, dtype=tf.float64)
+        vb = tf.Variable(0.0, dtype=tf.float64)
+
+        def get_loss():
+            yhat = va * tX + vb
+            loss = tf.losses.mean_squared_error(ty, yhat)
+            return loss
+
+        def get_train_op():
+            # We have to create the model and loss inside the train op
+            loss = get_loss()
+            return tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+
+        def body():
+            return get_train_op()
+
+        n = 5000
+
+        train_loop = repeat(body, n)
+
+        sess = tf.InteractiveSession()
+        sess.run(tf.global_variables_initializer())
+
+        with self.test_session() as sess:
+
+            sess.run(tf.global_variables_initializer())
+            sess.run(train_loop)
+
+            opt_a = va.eval()
+            opt_b = vb.eval()
+
+            final_loss = get_loss().eval()
+
+        print('Optimised a : %0.2f' % opt_a)
+        print('Optimised b : %0.2f' % opt_b)
+        print('Final Loss : %0.2f' % final_loss)
+
+        self.assertAlmostEqual(slope, opt_a, places=3)
+        self.assertAlmostEqual(intercept, opt_b, places=3)
